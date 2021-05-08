547ebb9063091f0a54d82cd108909a32255c197d
ATQQ <2604395430@qq.com>
2021-05-08 11:00:24 +0800

    feat: 移除控制导航栏收缩的组件，添加刷新日志按钮

--- src/pages/dashboard/manage/index.vue
+++ src/pages/dashboard/manage/index.vue
@@ -1,10 +1,10 @@
 <template>
   <div class="monitor">
     <div class="navs">
-      <el-radio-group v-model="isCollapse" style="margin-bottom: 20px;">
+      <!-- <el-radio-group v-model="isCollapse" style="margin-bottom: 20px;">
         <el-radio-button :label="false">展开</el-radio-button>
         <el-radio-button :label="true">收起</el-radio-button>
-      </el-radio-group>
+      </el-radio-group> -->
       <el-menu
         default-active="overview"
         class="el-menu-nav"
