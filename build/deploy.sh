02a871094346b66e2d2ab355d3b2bc84af4586b1
sugar <engineerzjl@foxmail.com>
2022-02-13 22:36:41 +0800

    chore: 修改部署脚本

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,8 +1,10 @@
 # /bin/bash
-compressFile="ep2.tar.gz"        # 压缩后的文件名
+compressFile="ep-clinet.tar.gz"        # 压缩后的文件名
 user="root"                         # 远程登录用户
 origin="sugarat.top"                   # 远程登录origin
-targetDir="/www/wwwroot/ep2.sugarat.top"     # 目标目录
+targetDir="/www/wwwroot/ep.sugarat.top"     # 目标目录
+# echo "开始-----上传"
+# scp ${compressFile} root@sugarat.top:./
 echo "开始-----部署"
 ssh -p22 ${user}@${origin} "tar -zvxf ${compressFile} -C ${targetDir}"
 echo "清理-----临时的文件"
