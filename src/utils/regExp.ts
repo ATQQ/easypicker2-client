74d9a8e3e0027bb57d85828c4cdf9d6781dfb673
sugar <engineerzjl@foxmail.com>
2022-04-27 18:40:41 +0800

    feat: 正则放开对手机号的限制,1开头即可

--- src/utils/regExp.ts
+++ src/utils/regExp.ts
@@ -1,5 +1,5 @@
 // 手机号
-export const rMobilePhone = /^(13[0-9]|14[5|7]|15[0|1|2|3|5|6|7|8|9]|16[0-9]|17[0-9]|18[0|1|2|3|5|6|7|8|9])\d{8}$/
+export const rMobilePhone = /^1\d{10}$/
 
 // 账号
 export const rAccount = /^(\d|[a-zA-Z]){4,11}$/
