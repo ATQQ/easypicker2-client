eb6ea16e18d2f23259c296d5159e6d67f2aeb379
sugar <engineerzjl@foxmail.com>
2022-05-01 23:42:12 +0800

    WIP: 支持设置批注消息

--- src/pages/dashboard/tasks/components/CreateTask.vue
+++ src/pages/dashboard/tasks/components/CreateTask.vue
@@ -1,19 +1,14 @@
 <template>
   <div>
     <div class="btn-area">
-      <el-button
-        size="large"
-        type="primary"
-        :plain="!isShowCreateTask"
-        @click="
+      <el-button size="large" type="primary" :plain="!isShowCreateTask" @click="
         isShowCreateTask =
-          !isShowCreateTask
-        "
-      >
+        !isShowCreateTask
+      ">
         {{
-          isShowCreateTask
-            ? '关闭创建面板'
-            : '创建收集任务'
+            isShowCreateTask
+              ? '关闭创建面板'
+              : '创建收集任务'
         }}
       </el-button>
     </div>
@@ -24,78 +19,70 @@
         <el-button @click="createTask" :type="taskName?.length ? 'success' : 'default'">确定</el-button>
       </div>
     </div>
+    <tip class="p10">
+      点击
+      <el-icon>
+        <Menu />
+      </el-icon> 可以进一步的调整任务
+    </tip>
+    <tip>
+      支持截止时间，自动文件重命名，名单限制，批注，模板。。。🚀
+    </tip>
   </div>
 </template>
-<script lang="ts">
+<script lang="ts" setup>
 import { ElMessage } from 'element-plus'
 import {
   computed,
-  defineComponent,
   ref,
 } from 'vue'
 import { useStore } from 'vuex'
+import { Menu } from '@element-plus/icons-vue'
+import Tip from './infoPanel/tip.vue'
 
-export default defineComponent({
-  name: 'createTask',
-  props: {
-    activeCategoryKey: {
-      type: String,
-      default: 'default',
-    },
-  },
-  setup(props) {
-    const $store = useStore()
-    const categorys = computed(
-      () => $store.state.category
-        .categoryList,
-    )
-    const categoryName = computed(
-      () => {
-        if (
-          props.activeCategoryKey
-          === 'default'
-        ) {
-          return '默认'
-        }
-        return categorys.value.find(
-          (v: any) => v.k
-            === props.activeCategoryKey,
-        ).name
-      },
-    )
-    // 任务相关
-    const isShowCreateTask = ref(false)
-    const taskName = ref('')
-    const createTask = () => {
-      if (!taskName.value.trim()) {
-        ElMessage.warning('不能为空')
-        return
-      }
-      $store
-        .dispatch('task/createTask', {
-          name: taskName.value,
-          category:
-            props.activeCategoryKey,
-        })
-        .then(() => {
-          ElMessage.success('创建成功')
-        })
-      taskName.value = ''
-    }
-    return {
-      isShowCreateTask,
-      taskName,
-      createTask,
-      categoryName,
-    }
+const props = defineProps({
+  activeCategoryKey: {
+    type: String,
+    default: 'default',
   },
 })
+const $store = useStore()
+const categorys = computed(() => $store.state.category
+  .categoryList)
+const categoryName = computed(() => {
+  if (props.activeCategoryKey
+        === 'default') {
+    return '默认'
+  }
+  return categorys.value.find((v: any) => v.k
+        === props.activeCategoryKey).name
+})
+// 任务相关
+const isShowCreateTask = ref(false)
+const taskName = ref('')
+const createTask = () => {
+  if (!taskName.value.trim()) {
+    ElMessage.warning('不能为空')
+    return
+  }
+  $store
+    .dispatch('task/createTask', {
+      name: taskName.value,
+      category: props.activeCategoryKey,
+    })
+    .then(() => {
+      ElMessage.success('创建成功')
+    })
+  taskName.value = ''
+}
+
 </script>
 <style scoped>
 .btn-area {
   display: flex;
   justify-content: center;
 }
+
 .input-container {
   margin: 15px auto;
   max-width: 600px;
