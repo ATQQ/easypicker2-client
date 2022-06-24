ac2ca4309fc48c848d7c26e465cceab0a20dc3aa
sugar <engineerzjl@foxmail.com>
2022-06-24 10:25:35 +0800

    feat:身份核验弹窗交互改为直接输入确认

--- src/components/InfosForm/index.vue
+++ src/components/InfosForm/index.vue
@@ -1,18 +1,19 @@
 <template>
-    <el-form :disabled="disabled" label-width="100px">
+    <el-form :disabled="disabled" :label-position="labelPosition || 'right'" label-width="100px">
         <el-form-item class="ellipsis" v-for="(
                 info, idx
               ) in infos" :key="idx" :label="info.type === 'text' ? '' : info.text">
-              <div class="fixed-text" v-if="info.type==='text'">
+            <div class="fixed-text" v-if="info.type === 'text'">
                 {{ info.text }}
-              </div>
+            </div>
             <el-input v-if="info.type === 'input'" :maxlength="
                 maxInputLength
             " clearable show-word-limit :placeholder="`请输入${info.text}`" v-model="info.value"></el-input>
             <el-radio-group v-if="info.type === 'radio'" v-model="info.value">
                 <el-radio v-for="(r, idx) in info.children" :key="idx" :label="r.text">{{ r.text }}</el-radio>
             </el-radio-group>
-            <el-select default-first-option clearable filterable v-if="info.type === 'select'" v-model="info.value" :placeholder="`请选择${info.text}`">
+            <el-select default-first-option clearable filterable v-if="info.type === 'select'" v-model="info.value"
+                :placeholder="`请选择${info.text}`">
                 <el-option v-for="(r, idx) in info.children" :key="idx" :label="r.text" :value="r.text" />
             </el-select>
         </el-form-item>
@@ -22,6 +23,7 @@
 defineProps<{
     infos: InfoItem[]
     disabled: boolean
+    labelPosition?: 'left' | 'right' | 'top'
 }>()
 
 const maxInputLength = +import.meta.env
@@ -29,7 +31,7 @@ const maxInputLength = +import.meta.env
 
 </script>
 <style  scoped lang="scss">
-.fixed-text{
+.fixed-text {
     background-color: #f5f7fa;
     padding: 0 10px;
     width: 100%;
