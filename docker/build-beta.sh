d70a5db32849907160a9dc84d151af266423930a
sugar <engineerzjl@foxmail.com>
2023-10-22 18:45:27 +0800

    chore: docker 脚本更新

--- docker/build-beta.sh
+++ docker/build-beta.sh
@@ -1,14 +1,2 @@
-# 目录准备
-if [ ! -d "./client" ]; then
-  mkdir ./client
-fi
-
-# 拷贝服务端资源(不在当前项目里，以后优化)
-cp -rf ../dist ./client/dist
-
-# 拷贝服务端资源(不在当前项目里，以后优化)
-cp -rf ./../../easypicker2-server/dist ./server
-cp -rf ./../../easypicker2-server/package.json ./server
-cp -rf ./../../easypicker2-server/pnpm-lock.yaml ./server
-
+bash ./bash.sh
 docker build -t sugarjl/easypicker:beta .
--- docker/build-version.mjs
+++ docker/build-version.mjs
@@ -1,15 +1,4 @@
-import fs from 'fs'
 import pkg from '../package.json' assert { type: 'json' }
 
-// 目录准备
-if (!fs.existsSync('./client')) {
-  fs.mkdirSync('./client')
-}
-
-// 拷贝服务端资源(不在当前项目里，以后优化)
-await $`cp -rf ../dist ./client/dist`
-// 拷贝服务端资源(不在当前项目里，以后优化)
-await $`cp -rf ./../../easypicker2-server/dist ./server`
-await $`cp -rf ./../../easypicker2-server/package.json ./server`
-await $`cp -rf ./../../easypicker2-server/pnpm-lock.yaml ./server`
+await $`bash ./bash.sh`
 await $`docker build -t sugarjl/easypicker:${pkg.version} .`
