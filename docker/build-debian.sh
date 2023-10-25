06d9b978835c3e002d19bd2df1adfe385e5a2356
sugar <engineerzjl@foxmail.com>
2023-10-25 21:31:36 +0800

    feat: 更新debian 镜像

--- docker/build-debian.sh
+++ docker/build-debian.sh
@@ -1,1 +1,1 @@
-docker buildx build -f debian.Dockerfile -t sugarjl/debian:test --platform=linux/arm64,linux/amd64 . --push
+docker buildx build -f debian.Dockerfile -t sugarjl/debian:latest --platform=linux/arm64,linux/amd64 . --push
