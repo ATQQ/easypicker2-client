44ee011c8322106e7443a6645b20587966d7f6be
sugar <engineerzjl@foxmail.com>
2023-12-10 13:55:05 +0800

    style: refactor Praise component layout and formatting

--- docs/src/components/Praise.vue
+++ docs/src/components/Praise.vue
@@ -9,8 +9,15 @@
       <tbody>
         <tr>
           <td v-for="(v, idx) in praiseImg" :key="idx">
-            <el-image hide-on-click-modal preview-teleported style="width: 200px; height: 200px" :src="v.url"
-              :preview-src-list="praiseImg.map(v => v.url)" :initial-index="idx" fit="cover" />
+            <el-image
+              hide-on-click-modal
+              preview-teleported
+              style="width: 200px; height: 200px"
+              :src="v.url"
+              :preview-src-list="praiseImg.map((v) => v.url)"
+              :initial-index="idx"
+              fit="cover"
+            />
           </td>
         </tr>
       </tbody>
@@ -18,28 +25,23 @@
   </span>
 </template>
 <script setup lang="ts">
-import {
-  ElImage,
-} from 'element-plus'
-import {
-  reactive,
-} from 'vue'
+import { ElImage } from 'element-plus'
+import { reactive } from 'vue'
 
 const praiseImg = reactive([
   {
     url: 'https://img.cdn.sugarat.top/mdImg/MTY1MTU0NzQ0MjMzNA==651547442334',
-    title: '微信',
+    title: '微信'
   },
   {
     url: 'https://img.cdn.sugarat.top/mdImg/MTY0Nzc1NTYyOTE5Mw==647755629193',
-    title: '微信赞赏',
+    title: '微信赞赏'
   },
   {
     url: 'https://img.cdn.sugarat.top/mdImg/MTY1MTU0NzQyOTg0OA==651547429848',
-    title: '支付宝',
-  },
+    title: '支付宝'
+  }
 ])
-
 </script>
 <style scoped>
 .praise {
