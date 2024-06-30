f1f268165f2e11535225afc80a7bbed20ef3adeb
sugar <engineerzjl@foxmail.com>
2024-06-30 17:04:26 +0800

    feat: 用户列表多样化排序支持

--- components.d.ts
+++ components.d.ts
@@ -39,6 +39,7 @@ declare module 'vue' {
     ElSwitch: typeof import('element-plus/es')['ElSwitch']
     ElTable: typeof import('element-plus/es')['ElTable']
     ElTableColumn: typeof import('element-plus/es')['ElTableColumn']
+    'ElTableColumn``': typeof import('element-plus/es')['ElTableColumn``']
     ElTabPane: typeof import('element-plus/es')['ElTabPane']
     ElTabs: typeof import('element-plus/es')['ElTabs']
     ElTag: typeof import('element-plus/es')['ElTag']
