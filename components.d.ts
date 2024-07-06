ba144f460a540c06f359c9c5540fa8bad467a7de
sugar <engineerzjl@foxmail.com>
2024-07-06 21:56:56 +0800

    chore: build dts

--- components.d.ts
+++ components.d.ts
@@ -39,7 +39,6 @@ declare module 'vue' {
     ElSwitch: typeof import('element-plus/es')['ElSwitch']
     ElTable: typeof import('element-plus/es')['ElTable']
     ElTableColumn: typeof import('element-plus/es')['ElTableColumn']
-    'ElTableColumn``': typeof import('element-plus/es')['ElTableColumn``']
     ElTabPane: typeof import('element-plus/es')['ElTabPane']
     ElTabs: typeof import('element-plus/es')['ElTabs']
     ElTag: typeof import('element-plus/es')['ElTag']
