fe6aa93210d4a7ec70734de39d694a35826c47c1
sugar <engineerzjl@foxmail.com>
2024-07-07 15:35:28 +0800

    style: lint code

--- src/pages/dashboard/tasks/components/infoPanel/tipInfo.vue
+++ src/pages/dashboard/tasks/components/infoPanel/tipInfo.vue
@@ -1,62 +1,9 @@
-<template>
-  <div class="tc ddl">
-    <tip
-      :imgs="[
-        'https://img.cdn.sugarat.top/mdImg/MTY1MTQ5NjI2OTI0MQ==651496269241'
-      ]"
-      >设置注意事项，供用户提交时查看</tip
-    >
-    <tip>注意控制字数和换行，避免展示异常，建议内容不超过5行</tip>
-    <div class="tc flex fc fac">
-      <el-input
-        v-model="textValue"
-        :rows="5"
-        clearable
-        :max="500"
-        show-word-limit
-        type="textarea"
-        placeholder="请输入要展示的批注信息"
-      />
-    </div>
-    <div class="p10">
-      <el-button size="default" @click="updateTip" type="success"
-        >保存</el-button
-      >
-      <el-button size="default" @click="textValue = ''" type="danger"
-        >清空</el-button
-      >
-    </div>
-    <tip v-if="needSave"> 有变动记得保存 </tip>
-    <tip> 可以设置图片啦↓ 最多3张 </tip>
-    <el-upload
-      accept="image/*"
-      :limit="MaxImgCount"
-      v-model:file-list="imageList"
-      action=""
-      ref="imgUpload"
-      list-type="picture-card"
-      :on-change="handleChangeFile"
-      :on-exceed="handleExceedFile"
-      :on-preview="handlePictureCardPreview"
-      :on-remove="handleRemove"
-      :auto-upload="false"
-    >
-      <el-icon><Plus /></el-icon>
-    </el-upload>
-    <ElImageViewer
-      hide-on-click-modal
-      :initial-index="previewIdx"
-      :url-list="previewList"
-      @close="imageViewerVisible = false"
-      teleported
-      v-if="imageViewerVisible"
-    />
-  </div>
-</template>
+<!-- eslint-disable ts/ban-ts-comment -->
 <script lang="ts" setup>
-import { ref, watch, computed, reactive } from 'vue'
+import { computed, reactive, ref, watch } from 'vue'
 import { Plus } from '@element-plus/icons-vue'
-import { ElMessage, UploadProps, UploadUserFile } from 'element-plus'
+import type { UploadProps, UploadUserFile } from 'element-plus'
+import { ElMessage } from 'element-plus'
 import { updateTaskInfo } from '../../public'
 import Tip from './tip.vue'
 import { FileApi, PublicApi, TaskApi } from '@/apis'
@@ -67,12 +14,12 @@ const props = defineProps({
   tip: {
     type: String,
     default: '',
-    required: false
+    required: false,
   },
   k: {
     type: String,
-    default: ''
-  }
+    default: '',
+  },
 })
 const textValue = ref('')
 
@@ -84,14 +31,13 @@ const tipData = reactive<{
   }[]
 }>({
   text: '',
-  imgs: []
+  imgs: [],
 })
 const MaxImgCount = ref(3)
 const imageList = ref<UploadUserFile[]>([])
 const previewList = computed(() => {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  return imageList.value.map((v) => v!.preview || v.url)
+  // @ts-expect-error
+  return imageList.value.map(v => v!.preview || v.url)
 })
 const previewIdx = ref(0)
 
@@ -106,27 +52,28 @@ watch(
       imageList.value = tipData.imgs.map((v) => {
         return {
           ...v,
-          url: 'https://img.cdn.sugarat.top/mdImg/MTY3NzkxMDI1NTU1Nw==20140524124237518.gif'
+          url: 'https://img.cdn.sugarat.top/mdImg/MTY3NzkxMDI1NTU1Nw==20140524124237518.gif',
         }
       })
       if (imageList.value.length) {
         // 异步填充url
         PublicApi.getTipImageUrl(
           props.k,
-          imageList.value.map((v) => ({
+          imageList.value.map(v => ({
             uid: v.uid,
-            name: v.name
-          }))
+            name: v.name,
+          })),
         ).then((v) => {
           v.data.forEach((url, idx) => {
             imageList.value[idx].url = url.cover
             Object.assign(imageList.value[idx], {
-              preview: url.preview
+              preview: url.preview,
             })
           })
         })
       }
-    } catch {
+    }
+    catch {
       tipData.text = props.tip || ''
       tipData.imgs = []
       imageList.value = []
@@ -135,18 +82,19 @@ watch(
     // 外部变动
     if (tipData.text) {
       textValue.value = tipData.text
-    } else {
+    }
+    else {
       textValue.value = ''
     }
   },
   {
-    immediate: true
-  }
+    immediate: true,
+  },
 )
 const needSave = computed(() => tipData.text !== textValue.value)
 
 // 更新批注信息
-const updateTip = (notify = true) => {
+function updateTip(notify = true) {
   if (tipData.text !== textValue.value) {
     tipData.text = textValue.value
   }
@@ -154,7 +102,7 @@ const updateTip = (notify = true) => {
 }
 
 const imageViewerVisible = ref(false)
-const handleChangeFile = (file: UploadUserFile) => {
+function handleChangeFile(file: UploadUserFile) {
   if (!props.k) {
     return
   }
@@ -168,29 +116,87 @@ const handleChangeFile = (file: UploadUserFile) => {
         success() {
           tipData.imgs.push({
             uid,
-            name
+            name,
           })
           updateTip()
-        }
+        },
       })
     })
   }
 }
 const handleRemove: UploadProps['onRemove'] = (file) => {
   const { uid, name } = file
-  const idx = tipData.imgs.findIndex((v) => v.uid === uid)
+  const idx = tipData.imgs.findIndex(v => v.uid === uid)
   tipData.imgs.splice(idx, 1)
   updateTip()
   TaskApi.delTipImage(props.k, uid, name)
 }
 
-const handlePictureCardPreview = (file) => {
+function handlePictureCardPreview(file) {
   imageViewerVisible.value = true
-  const idx = imageList.value.findIndex((v) => v.uid === file.uid)
+  const idx = imageList.value.findIndex(v => v.uid === file.uid)
   previewIdx.value = idx
 }
 // 超出选择的文件个数
-const handleExceedFile = () => {
+function handleExceedFile() {
   ElMessage.error(`只能选择${MaxImgCount.value}个图片,可删除后重新选择`)
 }
 </script>
+
+<template>
+  <div class="tc ddl">
+    <Tip
+      :imgs="[
+        'https://img.cdn.sugarat.top/mdImg/MTY1MTQ5NjI2OTI0MQ==651496269241',
+      ]"
+    >
+      设置注意事项，供用户提交时查看
+    </Tip>
+    <Tip>注意控制字数和换行，避免展示异常，设置完记得预览一下，再投放</Tip>
+    <div class="tc flex fc fac">
+      <el-input
+        v-model="textValue"
+        :rows="5"
+        clearable
+        :max="500"
+        show-word-limit
+        type="textarea"
+        placeholder="请输入要展示的批注信息"
+      />
+    </div>
+    <div class="p10">
+      <el-button size="default" type="success" @click="updateTip">
+        保存
+      </el-button>
+      <el-button size="default" type="danger" @click="textValue = ''">
+        清空
+      </el-button>
+    </div>
+    <Tip v-if="needSave">
+      有变动记得保存
+    </Tip>
+    <Tip> 可以设置图片啦↓ 最多3张 </Tip>
+    <el-upload
+      v-model:file-list="imageList"
+      accept="image/*"
+      :limit="MaxImgCount"
+      action=""
+      list-type="picture-card"
+      :on-change="handleChangeFile"
+      :on-exceed="handleExceedFile"
+      :on-preview="handlePictureCardPreview"
+      :on-remove="handleRemove"
+      :auto-upload="false"
+    >
+      <el-icon><Plus /></el-icon>
+    </el-upload>
+    <ElImageViewer
+      v-if="imageViewerVisible"
+      hide-on-click-modal
+      :initial-index="previewIdx"
+      :url-list="previewList"
+      teleported
+      @close="imageViewerVisible = false"
+    />
+  </div>
+</template>
